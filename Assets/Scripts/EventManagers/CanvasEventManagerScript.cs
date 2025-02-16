e9c8c9b3ec8a2283f2f9c04a9a727a8102166bba
tomse <tom.selecky@gmail.com>
2025-02-16 22:25:56 +0100

    event on GetMouseButtonDown

--- Assets/Scripts/EventManagers/CanvasEventManagerScript.cs
+++ Assets/Scripts/EventManagers/CanvasEventManagerScript.cs
@@ -11,11 +11,22 @@ namespace EventManagers
             EventButtonRightPointerDown,
             EventButtonRightPointerUp,
             EventButtonHornsClick;
+        
+        public static event Action<Vector3> EventOnScreenTap;
 
         public static void CallEventButtonLeftPointerDown() => EventButtonLeftPointerDown?.Invoke();
         public static void CallEventButtonLeftPointerUp() => EventButtonLeftPointerUp?.Invoke();
         public static void CallEventButtonRightPointerUp() => EventButtonRightPointerUp?.Invoke();
         public static void CallEventButtonRightPointerDown() => EventButtonRightPointerDown?.Invoke();
         public static void CallEventButtonHornsClick() => EventButtonHornsClick?.Invoke();
+        private static void CallEventOnScreenTap(Vector3 position) => EventOnScreenTap?.Invoke(position);
+        
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                CallEventOnScreenTap(Input.mousePosition);
+            }
+        }
     }
 }

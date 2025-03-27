b142e39ec030142959e9240b164cc11933fe4d8e
Tomáš Selecký <tomas.selecky@cleverlance.com>
2025-03-27 20:31:23 +0100

    intercepting mouse click

--- Assets/Scripts/EventManagers/CanvasEventManagerScript.cs
+++ Assets/Scripts/EventManagers/CanvasEventManagerScript.cs
@@ -1,17 +1,49 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace EventManagers
 {
     public class CanvasEventManagerScript : MonoBehaviour
     {
+        private void Update()
+        {
+            if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                // The new Input System stores the position in a Vector2, convert it to Vector3 if needed.
+                print("00000000000000000000000000000000000000000000000000000000");
+            }
+
+
+            // Check if a touchscreen is available.
+            if (Touchscreen.current == null) return;
+            // Iterate over all active touches.
+            if (Touchscreen.current.touches.Count > 0)
+            {
+                print("111111111111111111111111111111111111111111111111111111");
+            }
+
+            ;
+
+
+            // foreach (var touch in Touchscreen.current.touches)
+            // {
+            //     // When the touch is first pressed down.
+            //     if (touch.press.wasPressedThisFrame)
+            //     {
+            //         // Convert the touch position (Vector2) to Vector3 if needed.
+            //         CallEventOnScreenTap(touch.position.ReadValue());
+            //     }
+            // }
+        }
+
         public static event Action
             EventButtonLeftPointerDown,
             EventButtonLeftPointerUp,
             EventButtonRightPointerDown,
             EventButtonRightPointerUp,
             EventButtonHornsClick;
-        
+
         public static event Action<Vector3> EventOnScreenTap;
 
         public static void CallEventButtonLeftPointerDown() => EventButtonLeftPointerDown?.Invoke();
@@ -20,13 +52,5 @@ namespace EventManagers
         public static void CallEventButtonRightPointerDown() => EventButtonRightPointerDown?.Invoke();
         public static void CallEventButtonHornsClick() => EventButtonHornsClick?.Invoke();
         private static void CallEventOnScreenTap(Vector3 position) => EventOnScreenTap?.Invoke(position);
-        
-        private void Update()
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                CallEventOnScreenTap(Input.mousePosition);
-            }
-        }
     }
 }
